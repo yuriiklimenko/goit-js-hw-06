2fa0a71b7f021c8df73a433e3ed29d26457d15cb
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-10-01 10:29:51 +0300

    change six task

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,12 @@ const dataLength = Number.parseInt(inputRef.dataset.length);
 
 inputRef.addEventListener('blur', validationHandler);
 
-function validationHandler({ target: { value } }) {
-  value.length === dataLength
-    ? toggleValidation('valid', 'invalid')
-    : toggleValidation('invalid', 'valid');
-}
-
-function toggleValidation(valid, invalid) {
-  inputRef.classList.add(valid);
-  inputRef.classList.remove(invalid);
+function validationHandler(e) {
+  if (e.currentTarget.value.length === dataLength) {
+    e.currentTarget.classList.remove('invalid');
+    e.currentTarget.classList.add('valid');
+  } else {
+    e.currentTarget.classList.remove('valid');
+    e.currentTarget.classList.add('invalid');
+  }
 }
